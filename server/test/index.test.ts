fd8c8e5c96b8481764bd984df72837bbc7d2e82a
Jesse Shawl <jesse@jesse.sh>
2024-10-08 19:40:03 -0500

    delete by userId (#27)

--- server/test/index.test.ts
+++ server/test/index.test.ts
@@ -1,71 +1,106 @@
 import { SELF } from "cloudflare:test";
 import { describe, it, expect } from "vitest";
 
+const url = "https://example.com/127.0.0.1:8080/abcd";
+
 describe("remoji api", () => {
   it("responds to GET /:org/:id", async () => {
-    const response = await SELF.fetch(
-      "https://example.com/127.0.0.1:8080/abcd"
+    const response = await SELF.fetch(url);
+    expect(await response.json()).toEqual(
+      expect.objectContaining({
+        "😄": {
+          count: 2,
+        },
+      })
     );
-    expect(await response.json()).toStrictEqual({
-      "👀": {
-        count: 1,
-      },
-      "😄": {
-        count: 2,
-      },
-    });
   });
 
   it("responds to POST /:org/:id", async () => {
-    await SELF.fetch("https://example.com/127.0.0.1:8080/abcd", {
+    await SELF.fetch(url, {
       method: "POST",
       body: JSON.stringify({
         emoji: "😄",
         action: "increment",
+        userId: "jesse@jesse.sh",
       }),
     });
-    const response = await SELF.fetch(
-      "https://example.com/127.0.0.1:8080/abcd"
+    const response = await SELF.fetch(url);
+    expect(await response.json()).toEqual(
+      expect.objectContaining({
+        "😄": {
+          count: 3,
+        },
+      })
     );
-    expect(await response.json()).toStrictEqual({
-      "👀": {
-        count: 1,
-      },
-      "😄": {
-        count: 3,
-      },
-    });
   });
 
   it("responds to POST /:org/:id", async () => {
-    const response = await SELF.fetch(
-      "https://example.com/127.0.0.1:8080/abcd"
-    );
-    expect(await response.json()).toStrictEqual({
-      "👀": {
-        count: 1,
-      },
-      "😄": {
-        count: 2,
-      },
+    await SELF.fetch(url, {
+      method: "POST",
+      body: JSON.stringify({
+        emoji: "😄",
+        action: "increment",
+        userId: "jesse@jesse.sh",
+      }),
     });
-    await SELF.fetch("https://example.com/127.0.0.1:8080/abcd", {
+    const response = await SELF.fetch(url);
+    expect(await response.json()).toEqual(
+      expect.objectContaining({
+        "😄": {
+          count: 3,
+        },
+      })
+    );
+    await SELF.fetch(url, {
       method: "POST",
       body: JSON.stringify({
         emoji: "😄",
         action: "decrement",
+        userId: "jesse@jesse.sh",
       }),
     });
-    const response2 = await SELF.fetch(
-      "https://example.com/127.0.0.1:8080/abcd"
+    const response2 = await SELF.fetch(url);
+    expect(await response2.json()).toEqual(
+      expect.objectContaining({
+        "😄": {
+          count: 2,
+        },
+      })
     );
-    expect(await response2.json()).toStrictEqual({
-      "👀": {
-        count: 1,
-      },
-      "😄": {
-        count: 1,
-      },
+  });
+
+  it("ignores deletions by non-reactors", async () => {
+    await SELF.fetch(url, {
+      method: "POST",
+      body: JSON.stringify({
+        emoji: "😄",
+        action: "increment",
+        userId: "jesse-reactor@jesse.sh",
+      }),
     });
+    const response = await SELF.fetch(url);
+    expect(await response.json()).toEqual(
+      expect.objectContaining({
+        "😄": {
+          count: 3,
+        },
+      })
+    );
+    await SELF.fetch(url, {
+      method: "POST",
+      body: JSON.stringify({
+        emoji: "😄",
+        action: "decrement",
+        userId: "jesse-abuser@jesse.sh",
+      }),
+    });
+    const response2 = await SELF.fetch(url);
+    expect(await response2.json()).toEqual(
+      expect.objectContaining({
+        "😄": {
+          count: 3,
+        },
+      })
+    );
   });
 });
